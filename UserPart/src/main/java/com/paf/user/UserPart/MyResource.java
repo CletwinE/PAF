40d0b3944f57a2aa6e9d48a76dd536f80dbff876
Dilan8 <vasandarajdilan64@gmail.com>
2020-04-18 21:26:15 +0530

    delete and update

--- UserPart/src/main/java/com/paf/user/UserPart/MyResource.java
+++ UserPart/src/main/java/com/paf/user/UserPart/MyResource.java
@@ -17,9 +17,9 @@ public class MyResource {
      *
      * @return String that will be returned as a text/plain response.
      */
-    @GET
+    /*@GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
         return "Got it!";
-    }
+    }*/
 }
