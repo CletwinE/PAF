5ed40e0c28cf64c1983cb40d78bafce703ab0d1b
umai28 <umairajah03@gmail.com>
2020-04-19 15:24:49 +0530

    second commit

--- HospitalService/Hospital/src/main/java/com/Hospital/HospitalResources.java
+++ HospitalService/Hospital/src/main/java/com/Hospital/HospitalResources.java
@@ -11,8 +11,23 @@ import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.ProcessingException;
 import javax.ws.rs.Produces;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+
+
+
+
+
+
 
 
 @Path("/hospitals")
@@ -82,4 +97,28 @@ public class HospitalResources
 		return h;
 		
 	}
-}
+	
+	@GET
+	@Path("getDoctorHospital/{hid}")
+		
+	@Produces(MediaType.APPLICATION_JSON)
+	public final Response getHos(@PathParam("hid") String did) {
+		Client client = ClientBuilder.newClient();
+		WebTarget webTarget = client.target("http://localhost:8080/Hospital/webapi/hospitals/hospital").path("hospital").path(did);
+		
+		System.out.println("calling");
+		try
+		{
+			Response response = webTarget.request().get();
+			return response;
+		}
+		catch (ProcessingException e)
+		{
+			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+		}
+			
+		}
+		
+	}
+
+
